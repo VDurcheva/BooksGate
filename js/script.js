774569ad1fee22ba7975226d437196f5bc453d7a
VDurcheva <34923098+VDurcheva@users.noreply.github.com>
2017-12-28 22:03:10 +0200

    Added script.js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+	function myFunction() {
+		var x = document.getElementById("nav-primary");
+		if (x.className === "nav-primary") {
+			x.className += " responsive";
+		} else {
+			x.className = "nav-primary";
+		}
+    }
